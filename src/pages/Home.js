e242c0e1181c5b0e747448efb923fe612f64086a
BrianDyl <brianadyl@aol.com>
2020-05-06 16:50:49 -0400

    completed the service section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import {Link} from "react-router-dom";
+import Services from '../components/Services';
 const Home = () => {
     return (
+        <Fragment> 
         <Hero>
             <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
                 <Link to="/rooms" className="btn-primary">
@@ -11,8 +13,10 @@ const Home = () => {
                 </Link>
             </Banner>
         </Hero>
+        <Services/>
+        </Fragment>
     )
 }
 
-export default Home
+export default Home  
 
